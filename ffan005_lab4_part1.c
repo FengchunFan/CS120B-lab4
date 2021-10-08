e024366cbabfb564465af055fe290a1b92a9b185
FengchunFan <77016886+FengchunFan@users.noreply.github.com>
2021-10-08 13:02:39 -0700

    Update ffan005_lab4_part1.c

--- ffan005_lab4_part1.c
+++ ffan005_lab4_part1.c
@@ -12,3 +12,24 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+
+enum States {} state;
+
+void Tick(){
+  switch(state){
+  
+  }
+  
+  switch(state){
+     
+  }
+}
+
+void main(){
+DDRA = 0x00; PORTA = 0xFF;
+DDRB = 0xFF; PORTB = 0x00;
+  while (1) {
+Tick();
+}
+  return 1;
+}
