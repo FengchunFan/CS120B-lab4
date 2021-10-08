1e18b5750ac931658d49483a9e86392a8c2bb04a
FengchunFan <77016886+FengchunFan@users.noreply.github.com>
2021-10-08 16:02:04 -0700

    Update ffan005_lab4_part1.c

--- ffan005_lab4_part1.c
+++ ffan005_lab4_part1.c
@@ -13,15 +13,71 @@
 #include "simAVRHeader.h"
 #endif
 
-enum States {} state;
+// I feel this is very much like #3 in the in-class exercise, and I will borrow the template
+enum States {onPress, onRelease, offPress, offRelease, Start} state;
 
 void Tick(){
   switch(state){
+   case Start:
+    state = onPress;
+    break;
+    
+   case onPress:
+    if((PINA & 0x01) == 0x01){
+     state = onRelease;
+    } else {
+     state = onPress;
+    }
+    break;
+    
+   case onRelease:
+    if((PINA & 0x01) == 0x01){
+     state = offPress;
+    } else {
+     state = onRelease;
+    }
+    break;
+    
+   case offPress:
+    if((PINA & 0x01) == 0x01){
+     state = offRelease;
+    } else {
+     state = offPress;
+    }
+    break;
+    
+   case offRelease:
+    if((PINA & 0x01) == 0x01){
+     state = onPress;
+    } else {
+     state = offRelease;
+    }
+    break;
   
-  }
-  
+   case default:
+    state = Start;
+    break;
+    
   switch(state){
+   case Start:
+    PORTB = 0x01;
+    break;
      
+   case onPress:
+    PORTB = 0x02;
+    break;
+    
+   case onRelease:
+    PORTB = 0x02;
+    break;
+    
+   case offPress:
+    PORTB = 0x01;
+    break;
+    
+   case default:
+    PORTB = 0x01;
+    break;
   }
 }
 
