1e691291e9dcc52570edf9c5aabcb46fac9a6cb6
FengchunFan <77016886+FengchunFan@users.noreply.github.com>
2021-10-09 16:22:33 -0700

    Create ffan005_lab4_part2.c

---
+++ ffan005_lab4_part2.c
@@ -0,0 +1,105 @@
+
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum States {Start, Initial, Press, Increment, Decrement, Reset} state;
+
+void Tick(){
+  switch(state){
+    case Start:
+      state = Initial;
+      break;
+      
+    case Initial:
+      state = Press;
+      break;
+      
+    case Press:
+      if((PINA & 0x01) == 0x01){
+        state = Increment;
+      } else if ((PINA & 0x02) == 0x02){
+        state = Decrement;
+      } else if ((PINA & 0x03) == 0x03){
+        state = Reset;
+      }
+     break;
+        
+    case Increment:
+      if((PINA & 0x01) == 0x01){
+        state = Increment;
+      } else if ((PINA & 0x02) == 0x02){
+        state = Decrement;
+      } else if ((PINA & 0x03) == 0x03){
+        state = Reset;
+      }
+     break;
+      
+    case Decrement:
+      if((PINA & 0x02) == 0x02){
+        state = Decrement;
+      } else if ((PINA & 0x01) == 0x01){
+        state = Increment;
+      } else if ((PINA & 0x03) == 0x03){
+        state = Reset;
+      }
+     break;
+      
+    case Reset:
+      if((PINA & 0x03) == 0x03){
+        state = Reset;
+      } else if ((PINA & 0x02) == 0x02){
+        state = Decrement;
+      } else if ((PINA & 0x01) == 0x01){
+        state = Increment;
+      }
+     break;
+      
+    default:
+      state = Start;
+      break;
+  }
+  
+  switch(state){
+    case Start:
+      PORTC = 0x07;
+      break;
+      
+    case Initial:
+      PORTC = 0x07;
+      break;
+      
+    case Press:
+      break;
+      
+    case Increment:
+      if(PORTC < 0x08){
+        PORTC = PORTC + 1;
+      }
+      break;
+      
+    case Decrement:
+      if(PORTC > 0x00){
+        PORTC = PORTC - 1;
+      }
+     break;
+      
+    case Reset:
+      PORTC = 0x00;
+      break;
+      
+    default:
+      PORTC = 0x07;
+      break;
+  }
+}
+
+int main(void){
+DDRA = 0x00; PORTA = 0xFF;
+DDRC = 0xFF; PORTC = 0x00;
+  while (1) {
+Tick();
+}
+  return 1;
+}
