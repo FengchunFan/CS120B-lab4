35cb9a9ac5b61a3cc0de6c2b5cc4874759f9df34
FengchunFan <77016886+FengchunFan@users.noreply.github.com>
2021-10-10 15:36:31 -0700

    Update ffan005_lab4_part4.c

--- ffan005_lab4_part4.c
+++ ffan005_lab4_part4.c
@@ -1,3 +1,12 @@
+/*     Author: ffan005
+ *  Partner(s) Name:
+ *      Lab Section:
+ *      Assignment: Lab #  Exercise #
+ *      Exercise Description: [optional - include for your own benefit]
+ *
+ *      I acknowledge all content contained herein, excluding template or example
+ *      code, is my own original work.
+ */
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
